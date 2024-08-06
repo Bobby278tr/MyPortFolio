2f757cf4a34275f6ecd0d2b5705b306670c0a70f
Bobby Tripathi <80473945+Bobby278tr@users.noreply.github.com>
2024-08-07 02:07:22 +0530

    Adding navbar and other components and while setting the navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from './utils/Themes';
+import Navbar from "./Components/Navbar/Navbar"
+import HeroSection from './Components/HeroSection/HeroSection'
+import Skills from './Components/Skills/Skills'
+import Education from './Components/Education/Education'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const Body = styled.div`
+background-color: ${({ theme }) => theme.bg};
+width: 100%;
+overflow-x: hidden;
+height: 100%
+`
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
   return (
-    <div>Portfolio</div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+      <Navbar />
+      <Body>
+        <HeroSection />
+        <Wrapper>
+          <Skills />
+          <Education/>
+        </Wrapper>
+
+      </Body>
+      </Router>
+      
+    </ThemeProvider>
   );
 }
 
